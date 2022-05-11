17bf829d31290f05b1f835deb0196990bb16fd64
José Miguel <a19esrujo@iesgrancapitan.org>
2022-05-11 19:45:10 +0200

    Initial commit

---
+++ src/josemi/EdadIncorrectaException.java
@@ -0,0 +1,11 @@
+package josemi;
+
+public class EdadIncorrectaException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public EdadIncorrectaException(String string) {
+		super(string);
+	}
+
+}
